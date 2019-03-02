1573143a67fdfe9b6047e4171bc9d36343b6c8fb
anish-kunduru <anish-kunduru@users.noreply.github.com>
2019-03-01 23:52:14 -0600

    Added shell for kafka producer using baeldung example.

--- README.md
+++ README.md
@@ -5,9 +5,20 @@ Count the Top 100 words in Moby Dick using the Confluent Platform.
 1. Docker version 1.11 or later is installed and running.
 2. Docker Compose is installed. It is installed by default with Docker for Mac and Windows.
 3. Docker memory resource is allocated minimally at 8 GB.
+4. Maven is installed.
 
-## Starting the Demo
+### Starting the Demo
 1. From the root directory, `docker-compose up -d`. The first time you run this statement, it may take Docker a while to download the necessary components and build the necessary images.  
    Ensure that all the services are up and operational by running `docker-compose ps`.
 2. Navigate to Control Center web interface at: http://localhost:9021/  
    Note that it might take a couple minutes for the Control Center to come online. 
+3. Run the moby-dick-producer project by stepping into the sub-directory and running `mvn clean install spring-boot:run`.
+4. From the Control Center, you may see the data being produced to the *wordCountInput* topic by navigating to **Topics** in the left pane and double-clicking the *wordCountInput* topic from the main pane. Navigate to the **INSPECT** tab to see data as it flows in.
+   
+### Stopping the Demo
+1. To shut down docker processes and disconnect the volumes (flush data), `docker-compose down -v`.
+   
+#### Helpful References
+The following were used to create this quick-n'-dirty demo:
+* https://docs.confluent.io/current/quickstart/ce-docker-quickstart.html
+* https://www.baeldung.com/spring-kafka
---
+++ moby-dick-producer/src/main/java/com/baeldung/spring/kafka/KafkaApplication.java
@@ -0,0 +1,74 @@
+package com.baeldung.spring.kafka;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@SpringBootApplication
+public class KafkaApplication {
+
+    public static void main(String[] args) throws Exception {
+
+        ConfigurableApplicationContext context = SpringApplication.run(KafkaApplication.class, args);
+        
+        MessageProducer producer = context.getBean(MessageProducer.class);
+
+        /*
+         * Sending a Hello World message to topic 'baeldung'. 
+         * Must be recieved by both listeners with group foo
+         * and bar with containerFactory fooKafkaListenerContainerFactory
+         * and barKafkaListenerContainerFactory respectively.
+         * It will also be recieved by the listener with
+         * headersKafkaListenerContainerFactory as container factory
+         */
+        producer.sendMessage("Hello, World!");
+
+        context.close();
+    }
+
+    @Bean
+    public MessageProducer messageProducer() {
+        return new MessageProducer();
+    }
+
+    public static class MessageProducer {
+
+        @Autowired
+        private KafkaTemplate<String, String> kafkaTemplate;
+
+        @Value(value = "${message.topic.name}")
+        private String topicName;
+
+        public void sendMessage(String message) {
+            
+            ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, "moby-dick", message);
+            
+            future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+                @Override
+                public void onSuccess(SendResult<String, String> result) {
+                    System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+                }
+                @Override
+                public void onFailure(Throwable ex) {
+                    System.out.println("Unable to send message=[" + message + "] due to : " + ex.getMessage());
+                }
+            });
+        }
+    }
+}
